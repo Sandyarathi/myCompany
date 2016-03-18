d729bde3cfd519ccb9e1c4fa84da8f166cfa9de0
SandyarathiDas <sandyarathi1212@gmail.com>
2016-03-17 19:40:43 -0700

    Added @Configuration

--- src/main/java/com/myCompany/Application.java
+++ src/main/java/com/myCompany/Application.java
@@ -3,6 +3,9 @@ package com.myCompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
@@ -16,5 +19,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        return factory;
     }
 }

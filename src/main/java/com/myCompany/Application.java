99a34435779b9addddcaad73b28b18a92c6642fb
SandyarathiDas <sandyarathi1212@gmail.com>
2016-03-17 19:52:07 -0700

    Generated war

--- src/main/java/com/myCompany/Application.java
+++ src/main/java/com/myCompany/Application.java
@@ -3,8 +3,10 @@ package com.myCompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,15 +17,16 @@ import org.springframework.data.cassandra.repository.config.EnableCassandraRepos
 //@EnableAutoConfiguration
 //@Configuration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
-    public static void main(String[] args) {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
     
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-        return factory;
-    }
+    
 }
